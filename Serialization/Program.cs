b7e8215e4df83d7384933ec117f9cf91e52903ca
Irfan Muhammad_02 <72332713+Pixy02@users.noreply.github.com>
2021-08-29 21:04:47 +0700

    Add files via upload

---
+++ Serialization/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using PlayerStats;
+
+namespace Serialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // menginisialisasi data dan serializer  
+            serial serializer = new serial();
+            PlayerStats.DataPlayer data = new PlayerStats.DataPlayer();
+
+            // tujuan penyimpanan
+            string path = Directory.GetCurrentDirectory() + "/Data Player.bin";
+
+            // menginput data player
+            data.InputData();
+
+            // menjalankan serialisasi
+            serializer.SerialData(data, path);
+
+            // menutup data
+            Console.WriteLine("Press Any Key To Continue");
+            Console.ReadKey();
+        }
+    }
+}
