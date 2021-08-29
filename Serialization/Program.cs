259e45b3d423f3db1beb2e0c96b491961cc2d88b
Irfan Muhammad_02 <72332713+Pixy02@users.noreply.github.com>
2021-08-29 21:06:11 +0700

    Update Program.cs

--- Serialization/Program.cs
+++ Serialization/Program.cs
@@ -8,20 +8,15 @@ namespace Serialization
     {
         static void Main(string[] args)
         {
-            // menginisialisasi data dan serializer  
             serial serializer = new serial();
             PlayerStats.DataPlayer data = new PlayerStats.DataPlayer();
-
-            // tujuan penyimpanan
+            
             string path = Directory.GetCurrentDirectory() + "/Data Player.bin";
 
-            // menginput data player
             data.InputData();
 
-            // menjalankan serialisasi
             serializer.SerialData(data, path);
 
-            // menutup data
             Console.WriteLine("Press Any Key To Continue");
             Console.ReadKey();
         }
