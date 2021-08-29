82e33ffa7623af3d3560a705755382312139e80a
Irfan Muhammad_02 <72332713+Pixy02@users.noreply.github.com>
2021-08-29 20:55:13 +0700

    Update Program.cs

--- Deserialization/Program.cs
+++ Deserialization/Program.cs
@@ -7,20 +7,15 @@ namespace Deserialization
     class Program
     {
         static void Main(string[] args)
-        {
-            // initiate object deserializer
+        {    
             Deserializer deserializer = new Deserializer();
 
-            // set file path
             string path = Directory.GetCurrentDirectory() + "/Data Player.bin";
 
-            // run deserializer file
             PlayerStats.DataPlayer data = deserializer.DeserializeData(path);
 
-            // show player data
             data.ShowData();
 
-            // close program
             Console.WriteLine("Press Any Key To Continue");
             Console.ReadKey();
         }
