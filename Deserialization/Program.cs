d0faae4cc7b57c23777ee7bcab9b1464ea7f6901
Irfan Muhammad_02 <72332713+Pixy02@users.noreply.github.com>
2021-08-29 20:48:51 +0700

    Add files via upload

---
+++ Deserialization/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using PlayerStats;
+
+namespace Deserialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // initiate object deserializer
+            Deserializer deserializer = new Deserializer();
+
+            // set file path
+            string path = Directory.GetCurrentDirectory() + "/Data Player.bin";
+
+            // run deserializer file
+            PlayerStats.DataPlayer data = deserializer.DeserializeData(path);
+
+            // show player data
+            data.ShowData();
+
+            // close program
+            Console.WriteLine("Press Any Key To Continue");
+            Console.ReadKey();
+        }
+    }
+}
